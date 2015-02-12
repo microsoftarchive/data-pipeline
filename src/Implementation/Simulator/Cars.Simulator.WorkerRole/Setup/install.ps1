28f1c6ed95ce52fd11d27a6a79e6db851899b191
Larry Brader <lbrader@microsoft.com>
2015-02-11 16:25:49 -0800

    test - fixed issue with installer. Validate console, local and azure deployment

--- src/Implementation/Simulator/Cars.Simulator.WorkerRole/Setup/install.ps1
+++ src/Implementation/Simulator/Cars.Simulator.WorkerRole/Setup/install.ps1
@@ -1,4 +1,4 @@
-﻿& "$env:windir\Microsoft.NET\Framework64\v4.0.30319\InstallUtil.exe" ..\Microsoft.Practices.DataPipeline.Cars.Simulator.WorkerRole.dll
+﻿& "$env:windir\Microsoft.NET\Framework64\v4.0.30319\InstallUtil.exe" ..\Microsoft.Practices.DataPipeline.Cars.Simulator.dll
 & "$env:windir\System32\unlodctr.exe" /m:..\Microsoft.ServiceBus.MessagingPerformanceCounters.man
 & "$env:windir\System32\lodctr.exe" /M:..\Microsoft.ServiceBus.MessagingPerformanceCounters.man 
 
