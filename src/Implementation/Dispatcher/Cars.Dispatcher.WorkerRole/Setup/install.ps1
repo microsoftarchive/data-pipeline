574227c81d6522c7b0a7292fed99fe1b61d862cc
Larry Brader <lbrader@microsoft.com>
2015-02-11 16:35:19 -0800

    Updated all install.ps1 to point to the correct DLL for the counter installer

--- src/Implementation/Dispatcher/Cars.Dispatcher.WorkerRole/Setup/install.ps1
+++ src/Implementation/Dispatcher/Cars.Dispatcher.WorkerRole/Setup/install.ps1
@@ -1,4 +1,4 @@
-﻿& "$env:windir\Microsoft.NET\Framework64\v4.0.30319\InstallUtil.exe" ..\Microsoft.Practices.DataPipeline.Cars.Dispatcher.WorkerRole.dll
+﻿& "$env:windir\Microsoft.NET\Framework64\v4.0.30319\InstallUtil.exe" ..\Microsoft.Practices.DataPipeline.Dispatcher.dll
 & "$env:windir\System32\unlodctr.exe" /m:..\Microsoft.ServiceBus.MessagingPerformanceCounters.man
 & "$env:windir\System32\lodctr.exe" /M:..\Microsoft.ServiceBus.MessagingPerformanceCounters.man 
 
